e17795b03adf10b9c6ba0f8f298a322163b0b205
Jamie Thomas <jamie.matthew.thomas@gmail.com>
2026-06-17 22:45:24 +0100

    More puzzles

--- flagfill/flags.js
+++ flagfill/flags.js
@@ -6,12 +6,12 @@ const FLAGS_DATA = {
     ],
     "svg": `<path fill="#006a4e" d="M0 0h640v480H0z"/><circle cx="280" cy="240" r="160" fill="#f42a41"/>`
   },
-  "Denmark": {
+  "Barbados": {
     "shapes": [
-      { "id": "bg", "fill": "#c8102e" },
-      { "id": "cross", "fill": "#fff" }
+      { "id": "bg", "fill": "#00267f" },
+      { "id": "mid", "fill": "#ffc726" }
     ],
-    "svg": `<path fill="#c8102e" d="M0 0h640.1v480H0z"/><path fill="#fff" d="M205.7 0h68.6v480h-68.6z"/><path fill="#fff" d="M0 205.7h640.1v68.6H0z"/>`
+    "svg": `<path fill="#00267f" d="M0 0h640v480H0z"/><path fill="#ffc726" d="M213.3 0h213.4v480H213.3z"/><path id="bb-a" fill="#000001" d="M319.8 135.5c-7 19-14 38.6-29.2 53.7 4.7-1.6 13-3 18.2-2.8v79.5l-22.4 3.3c-.8 0-1-1.3-1-3-2.2-24.7-8-45.5-14.8-67-.5-2.9-9-14-2.4-12 .8 0 9.5 3.6 8.2 1.9a85 85 0 0 0-46.4-24c-1.5-.3-2.4.5-1 2.2 22.4 34.6 41.3 75.5 41.1 124 8.8 0 30-5.2 38.7-5.2v56.1H320l2.5-156.7z"/><use xlink:href="#bb-a" width="100%" height="100%" transform="matrix(-1 0 0 1 639.5 0)"/>`
   },
   "Colombia": {
     "shapes": [
@@ -21,6 +21,12 @@ const FLAGS_DATA = {
     ],
     "svg": `<g fill-rule="evenodd" stroke-width="1pt"><path fill="#ffe800" d="M0 0h640v480H0z"/><path fill="#00148e" d="M0 240h640v240H0z"/><path fill="#da0010" d="M0 360h640v120H0z"/></g>`
   },
+  "Denmark": {
+    "shapes": [
+      { "id": "bg", "fill": "#c8102e" }
+    ],
+    "svg": `<path fill="#c8102e" d="M0 0h640.1v480H0z"/><path fill="#fff" d="M205.7 0h68.6v480h-68.6z"/><path fill="#fff" d="M0 205.7h640.1v68.6H0z"/>`
+  },
   "Lithuania": {
     "shapes": [
       { "id": "top", "fill": "#fdb913" },
@@ -28,6 +34,20 @@ const FLAGS_DATA = {
       { "id": "bottom", "fill": "#c1272d" }
     ],
     "svg": `<g fill-rule="evenodd" stroke-width="1pt" transform="scale(.64143 .96773)"><rect width="1063" height="708.7" fill="#006a44" rx="0" ry="0" transform="scale(.93865 .69686)"/><rect width="1063" height="236.2" y="475.6" fill="#c1272d" rx="0" ry="0" transform="scale(.93865 .69686)"/><path fill="#fdb913" d="M0 0h997.8v164.6H0z"/></g>`
-  }
+  },
+  "Kenya": {
+    "shapes": [
+      { "id": "mid", "fill": "#bb0000" },
+      { "id": "bottom", "fill": "#006600" }
+    ],
+    "svg": `<defs><path id="ke-a" stroke-miterlimit="10" d="m-28.6 47.5 1.8 1 46.7-81c2.7-.6 4.2-3.2 5.7-5.8 1-1.8 5-8.7 6.7-17.7a58 58 0 0 0-11.9 14.7c-1.5 2.6-3 5.2-2.3 7.9z"/>  </defs>  <path fill="#fff" d="M0 0h640v480H0z"/>  <path fill="#000001" d="M0 0h640v144H0z"/>  <path fill="#006600" d="M0 336h640v144H0z"/>  <g id="ke-b" transform="matrix(3 0 0 3 320 240)">    <use xlink:href="#ke-a" width="100%" height="100%" stroke="#000"/>    <use xlink:href="#ke-a" width="100%" height="100%" fill="#fff"/>  </g>  <use xlink:href="#ke-b" width="100%" height="100%" transform="matrix(-1 0 0 1 640 0)"/>  <path fill="#bb0000" d="M640.5 168H377c-9-24-39-72-57-72s-48 48-57 72H-.2v144H263c9 24 39 72 57 72s48-48 57-72h263.5z"/>  <path id="ke-c" d="M377 312c9-24 15-48 15-72s-6-48-15-72c-9 24-15 48-15 72s6 48 15 72"/>  <use xlink:href="#ke-c" width="100%" height="100%" transform="matrix(-1 0 0 1 640 0)"/>  <g fill="#fff" transform="matrix(3 0 0 3 320 240)">    <ellipse rx="4" ry="6"/>    <path id="ke-d" d="M1 5.8s4 8 4 21-4 21-4 21z"/>    <use xlink:href="#ke-d" width="100%" height="100%" transform="scale(-1)"/>    <use xlink:href="#ke-d" width="100%" height="100%" transform="scale(-1 1)"/>    <use xlink:href="#ke-d" width="100%" height="100%" transform="scale(1 -1)"/>  </g>`
+  },
+  "Norway": {
+    "shapes": [
+      { "id": "bg", "fill": "#ed2939" },
+      { "id": "cross", "fill": "#002664" }
+    ],
+    "svg": `<path fill="#ed2939" d="M0 0h640v480H0z"/>  <path fill="#fff" d="M180 0h120v480H180z"/>  <path fill="#fff" d="M0 180h640v120H0z"/>  <path fill="#002664" d="M210 0h60v480h-60z"/>  <path fill="#002664" d="M0 210h640v60H0z"/>`
+  },
 }
 ;
