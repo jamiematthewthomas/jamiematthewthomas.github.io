58a06af75ab9658f58714a6fe7ba507123a045fa
Jamie Thomas <jamie.matthew.thomas@gmail.com>
2026-06-21 19:42:45 +0100

    Fixes to flags and puzzles

--- flagfill/puzzles.js
+++ flagfill/puzzles.js
@@ -6,10 +6,10 @@ const PUZZLES_DATA = [
   { "date": "2026-06-19", "flag": "Brazil", "shapeId": "diamond" },
   { "date": "2026-06-20", "flag": "Kenya", "shapeId": "mid" },
   { "date": "2026-06-21", "flag": "Norway", "shapeId": "cross" },
-  { "date": "2026-06-22", "flag": "Morocco", "shapeId": "star" },
-  { "date": "2026-06-23", "flag": "Anitgua and Barbuda", "shapeId": "cross" },
+  { "date": "2026-06-22", "flag": "Seychelles", "shapeId": "second_band" },
+  { "date": "2026-06-23", "flag": "Antigua and Barbuda", "shapeId": "middle" },
   { "date": "2026-06-24", "flag": "Cyprus", "shapeId": "map" },
-  { "date": "2026-06-25", "flag": "South Korea", "shapeId": "circle_lower" },
+  { "date": "2026-06-21", "flag": "South Korea", "shapeId": "circle_lower" },
   { "date": "2026-06-26", "flag": "India", "shapeId": "top" },
 ]
 ;
